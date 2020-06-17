1abb1f47c28d6daf5fe58aa487fd11933602c097
Krzysztof Pawliński <krzysztof.pawlinski1@gmail.com>
2020-06-17 23:37:31 +0200

    dodano interfejs uzytkownika

--- GMPDMPP/src/generatorChmuryPunktow.py
+++ GMPDMPP/src/generatorChmuryPunktow.py
@@ -11,10 +11,6 @@ from reportlab.lib.colors import red
 from Crypto.Random.random import randrange
 from matplotlib.colors import LogNorm
 
-
-
-
-
 rozdzielczosc_hisogram = 8
 rozdzielczoscKol=1
 rozdzielczoscProstokatow=1
@@ -35,11 +31,13 @@ wierzcholekBList=[]
 caloscX=[]
 caloscY=[]
 
+#do rysowania pliku pgm
+arrKrawedziX=[]
+arrKrawedziY=[]
 
 #
 #inicjowanie wczytania grafu
 #
-
 
 obiekt=grafListy.graf()
 obiekt.inicjuj('test.dat')
@@ -55,6 +53,22 @@ parametrB=obiekt.parametrPrzeszkodyB
 promienList=parametrA
 bokA=parametrA
 bokB=parametrB
+
+def wyswietlMenu():
+    print("Co chcesz zrobic?", flush=True)
+    print("0 - zakoncz prace programu")
+    print("1 - wyswietl przeszkody")
+    print("2 - wyswietl wykres wykonany w kde")
+    print("3 - wyswietl wykres wykonany w seaborn")
+    print("4 - wyswietl histogram wykonany przy pomocy biblioteki matplotlib")
+    print("5 - wyswietl wyswietl mape punktow wykonana przy pomocy gestosci prawdopodobienstwa")
+    print("6 - wyswietl punkty wylosowane przy nalozeniu funkcji rozkladu prawodopobienstwa")
+    print("7 - wyswietl histogram")
+    print("8 - wyswietl mape w formacie pgm")
+    print("9 - wyswietl mape prawdopodobienstwa (moze zajac pare minut..)")    
+
+ # 1 - przeszkody punkty niebieskie, 2 - zielony z konturami, 3 - ???, 4 - to z bokami, 5 - przeszkody punkty + niski potencjał na czerwono
+ # 6 - punkty duzo, 7 - histogram, 8 - nie chce sie policzyc ://, 9 - pgm     
 
 def obliczRozmiary():
 
@@ -101,6 +115,17 @@ def rysujProstokat(A,B):
     arrPunktowY.append(wspolrzednaYList[i]-B/2)
     arrPunktowX.append(wspolrzednaXList[i]+A/2)
     arrPunktowY.append(wspolrzednaYList[i]-B/2)
+
+    #DO ZNAJDYWANIA PUNKTOW PRZEJSCIOWYCH
+    arrKrawedziX.append(wspolrzednaXList[i]+A/2)
+    arrKrawedziY.append(wspolrzednaYList[i]+B/2)
+    arrKrawedziX.append(wspolrzednaXList[i]-A/2)
+    arrKrawedziY.append(wspolrzednaYList[i]+B/2)
+    arrKrawedziX.append(wspolrzednaXList[i]-A/2)
+    arrKrawedziY.append(wspolrzednaYList[i]-B/2)
+    arrKrawedziX.append(wspolrzednaXList[i]+A/2)
+    arrKrawedziY.append(wspolrzednaYList[i]-B/2)
+
     odlegloscA=A
     odlegloscB=B
     iteracjaA=1
@@ -112,6 +137,15 @@ def rysujProstokat(A,B):
             arrPunktowY.append(wspolrzednaYList[i]+B/2)
             arrPunktowX.append(wspolrzednaXList[i]-A/2+(x*2+1)*A/mianownik)
             arrPunktowY.append(wspolrzednaYList[i]-B/2)
+
+
+            #DO ZNAJDYWANIA PUNKTOW PRZEJSCIOWYCH
+            arrKrawedziX.append(wspolrzednaXList[i]-A/2+(x*2+1)*A/mianownik)
+            arrKrawedziY.append(wspolrzednaYList[i]+B/2)
+            arrKrawedziX.append(wspolrzednaXList[i]-A/2+(x*2+1)*A/mianownik)
+            arrKrawedziY.append(wspolrzednaYList[i]-B/2)
+      
+
         iteracjaA=iteracjaA+1
         odlegloscA=odlegloscA/2
     while odlegloscB>rozdzielczoscProstokatow:
@@ -121,6 +155,15 @@ def rysujProstokat(A,B):
             arrPunktowY.append(wspolrzednaYList[i]-B/2+(x*2+1)*B/mianownik)
             arrPunktowX.append(wspolrzednaXList[i]-A/2)
             arrPunktowY.append(wspolrzednaYList[i]-B/2+(x*2+1)*B/mianownik)    
+                
+
+            #DO ZNAJDYWANIA PUNKTOW PRZEJSCIOWYCH
+            arrKrawedziX.append(wspolrzednaXList[i]+A/2)
+            arrKrawedziY.append(wspolrzednaYList[i]-B/2+(x*2+1)*B/mianownik)
+            arrKrawedziX.append(wspolrzednaXList[i]-A/2)
+            arrKrawedziY.append(wspolrzednaYList[i]-B/2+(x*2+1)*B/mianownik)    
+        
+
         iteracjaB=iteracjaB+1
         odlegloscB=odlegloscB/2
 
@@ -143,7 +186,7 @@ def rysujProstokatv2(A,B):
 
 
 
-def rysujOkrag(promien):
+def rysujOkrag(promien, czyKrawedz):
     kat=0
     krok=90
     odleglosc=999
@@ -152,6 +195,15 @@ def rysujOkrag(promien):
             arrPunktowX.append(promien*math.cos(kat*math.pi/180)+wspolrzednaXList[i])
             arrPunktowY.append(promien*math.sin(kat*math.pi/180)+wspolrzednaYList[i])
             kat=kat+krok
+
+            
+            #DO ZNAJDYWANIA PUNKTOW PRZEJSCIOWYCH
+            if czyKrawedz=='y':
+                arrKrawedziX.append(promien*math.cos(kat*math.pi/180)+wspolrzednaXList[i])
+                arrKrawedziY.append(promien*math.sin(kat*math.pi/180)+wspolrzednaYList[i])
+                kat=kat+krok
+
+
         krok=krok/2
         kat=krok
         odleglosc=krok/360*2*math.pi*promien
@@ -174,17 +226,13 @@ for x in range(0, len(wierzcholkiPrzeszkodaList)):
         arrPunktowX.append(wspolrzednaXList[i])#srodek X
         arrPunktowY.append(wspolrzednaYList[i])#srodek Y
         #W tej funkcji dodawane sa punkty na okregu
-        rysujOkrag(promien)
+        #W tej funkcji dodawane sa punkty na okregu
+        rysujOkrag(promien,'y')
         odlegloscWSrodku=promien
         #W tym while dodawane sa punkty wewnatrz okregu (czyli punkty na okregu o mniejszym promieniu)
         iteracjaSrodka=1
-        while odlegloscWSrodku>rozdzielczoscKol:
-                for x in range (0,pow(2,(iteracjaSrodka-1))):
-                    rysujOkrag(promien*(2*x+1)/(pow(2,iteracjaSrodka)))
-                odlegloscWSrodku=odlegloscWSrodku/math.pi
-                iteracjaSrodka=iteracjaSrodka+1
-        #jesli wieksza niz rozdzielczosc to dodaj pomiedzy srodkiem a punktami na luku
-        #po wyjsciu z while (dobra rozdzielczosc w srodku) przeszkoda jest gotowa
+        for x in range (1,promien-1):
+            rysujOkrag(x,'n')
     else:#przeszkoda jest kwadratem
         A=bokA[i]
         B=bokB[i]
@@ -204,19 +252,63 @@ for i in range(0, len(arrPunktowX)):
     tablica2D[math.floor(arrPunktowX[i])][math.floor(arrPunktowY[i])]=1
 #print(tablica2D)
 
-"""
-# open file for writing 
-filename = 'x.pgm'
-with open(filename, 'w+') as f:
-    print('P1', file=f)
-    print(rozmiarX," ",rozmiarY,file=f) 
+tablica2DRaw=tablica2D.copy()
+
+def przygotujPlikPgm():
+    print("Wszystkie punkty: ",len(arrPunktowX),", punkty na krawedzi: ",len(arrKrawedziX))
+
     for i in range(0, rozmiarX):
+        print(i)
         for j in range(0, rozmiarY): 
-            print(tablica2D[i][j]," ",file=f,end="")
-        print(" ",file=f)"""
+            if tablica2DRaw[i][j]==0:
+                if i%3==1 and j%3==1:
+                    #na poczatku przypisuje jakas wysoka
+                    odlegloscOdWysokiego=10000
+                    ###for przejezdza po wszystkich punktach z arrPunktowX i arrPunktowY
+                    for x in range(0, len(arrKrawedziX)):
+                        #Dla każdego ze sprawdzanych punktów liczy odległość jeszcze raz
+                        odlegloscTmp=math.sqrt(((arrKrawedziX[x]-i)*(arrKrawedziX[x]-i))+((arrKrawedziY[x]-j)*(arrKrawedziY[x]-j)))
+                        if odlegloscTmp<odlegloscOdWysokiego:
+                            odlegloscOdWysokiego=odlegloscTmp
+                            #przypisz 1/wartość do odpowiedniego pola w tablicy 2D (może być * współczynnik)
+                            if odlegloscOdWysokiego!=0:
+                                wartosc=math.floor(255/(odlegloscOdWysokiego*0.1))
+                            if wartosc<255:
+                                tablica2DRaw[i][j]=wartosc
+                            else:
+                                tablica2DRaw[i][j]=255
+    for i in range(0, rozmiarX):
+        print(i)
+        for j in range(0, rozmiarY):
+            if tablica2DRaw[i][j]==0:
+                #KOPIUJE WARTOŚĆ Z SĄSIEDZTWA
+                if i%3==0 and j%3==0 and i<rozmiarX-1 and j<rozmiarY-1:#nie działa
+                    tablica2DRaw[i][j]=tablica2DRaw[i+1][j+1]
+                elif i%3==0 and j%3==1 and j<rozmiarY-1:#nie działa
+                    tablica2DRaw[i][j]=tablica2DRaw[i+1][j]
+                elif i%3==0 and j%3==2 and i!=0 and j<rozmiarY-1:
+                    tablica2DRaw[i][j]=tablica2DRaw[i+1][j-1]
+                elif i%3==1 and j%3==0 and i<rozmiarX-1:
+                    tablica2DRaw[i][j]=tablica2DRaw[i][j+1]
+                elif i%3==1 and j%3==2 and i!=0:
+                    tablica2DRaw[i][j]=tablica2DRaw[i][j-1]
+                elif i%3==2 and j%3==0 and j!=0 and i<rozmiarX-1:
+                    tablica2DRaw[i][j]=tablica2DRaw[i-1][j+1]
+                elif i%3==2 and j%3==1 and j!=0:
+                    tablica2DRaw[i][j]=tablica2DRaw[i-1][j]
+                elif i%3==2 and j%3==2 and j!=0 and i!=0:
+                    tablica2DRaw[i][j]=tablica2DRaw[i-1][j-1]
 
-
-
+    # open file for writing
+    filename = 'x.pgm'
+    with open(filename, 'w+') as f:
+        print('P2', file=f)
+        print(rozmiarX," ",rozmiarY,file=f)
+        print('255', file=f)
+        for i in reversed(range(0,rozmiarX)):
+            for j in range(0,rozmiarY): 
+                print(tablica2DRaw[j][i]," ",file=f,end="")
+            print(" ",file=f)
 
 ########### Niski potencjał
 
@@ -322,9 +414,6 @@ for i in range(rozmiarX):
 '''
 tutaj rozmycie lub wyżej
 '''
-
-
-
     
 def Tablica2DxRozdzielczosc(rozdzielczosc):
     
@@ -357,83 +446,105 @@ df['y'] = arrPunktowY
 
 df.head()
 
-fig, ax = plt.subplots()
+#fig, ax = plt.subplots()
 
-ax.set_xlim(rozmiary[0],rozmiary[1])
-ax.set_ylim(rozmiary[2],rozmiary[3])
+#ax.set_xlim(rozmiary[0],rozmiary[1])
+#ax.set_ylim(rozmiary[2],rozmiary[3])
 
-sns.kdeplot(df['x'],df['y'], n_levels=2, shade="True", ax=ax).set_title('Rozmieszczenie przeszkód')
+wyswietlMenu()
+wybor = int(input("Twoj wybor: "))
+while wybor!=0:
+    print(wybor)
+    if wybor==1:
+        #same punkty z nakladaniem sie
+        sns.lmplot( x="x", y="y", data=df, fit_reg=False)
+        plt.show()
 
-sns.kdeplot(df['x'],df['y'], n_levels=2, ax=ax)
+    elif wybor==2:
+        sns.kdeplot(df['x'],df['y'], n_levels=2, shade="True").set_title('Rozmieszczenie przeszkód')
+        sns.kdeplot(df['x'],df['y'], n_levels=2)
+        plt.show()
+    elif wybor==3:
+        sns.regplot(x=df['x'],y=df['y'], fit_reg=False)
+        plt.show()
+    elif wybor==4:
+        #ten z rozkladem punktow na bokach
+        sns.jointplot(x=df["x"], y=df["y"],n_levels=2, kind='kde')
+        plt.show()
+    elif wybor==5:
+        #
+        #niskie potencjaly
+        #
 
-sns.regplot(x=df['x'],y=df['y'], fit_reg=False, ax=ax)
+        f['x'] = arrNiskiX
+        f['y'] = arrNiskiY
 
+        f.head()
 
-#same punkty z nakladaniem sie
-sns.lmplot( x="x", y="y", data=df, fit_reg=False)
+        fi, a = plt.subplots()
+        #wykres rozmiejscowienia przeszkod i niskich potencjalow
 
-#ten z rozkladem punktow na bokach
+        sns.regplot(x=f['x'], y=f['y'], fit_reg=False,color='r', ax=a).set_title('Rozmieszczenie przeszkód(niebieski) i niskich potencjałów(czerwony)')
 
-sns.jointplot(x=df["x"], y=df["y"],n_levels=2, kind='kde')
+        sns.regplot(x=df['x'], y=df['y'], fit_reg=False, color='b', ax=a)
+        plt.show()
+    elif wybor==6:
+        #
+        #calosc
+        #
+        calosc['x'] = caloscX
+        calosc['y'] = caloscY
+        calosc.head()
 
+        '''
+        plt.streamplot(calosc['x'],calosc['y'])
+        streamplot - histogram przepływu'''
 
-#
-#niskie potencjaly
-#
+        #punkty
+        figure, axes = plt.subplots()
 
-f['x'] = arrNiskiX
-f['y'] = arrNiskiY
+        plt.title('Mapa prawdopodobieństwa punktowa')
 
-f.head()
+        plt.plot(caloscX,caloscY,'.',color='black')
+        plt.show()
+    elif wybor==7:
+        #
+        #calosc
+        #
+        calosc['x'] = caloscX
+        calosc['y'] = caloscY
+        calosc.head()
 
-fi, a = plt.subplots()
-#wykres rozmiejscowienia przeszkod i niskich potencjalow
+        #histogram 2D
+        figur, axis = plt.subplots()
 
-sns.regplot(x=f['x'], y=f['y'], fit_reg=False,color='r', ax=a).set_title('Rozmieszczenie przeszkód(niebieski) i niskich potencjałów(czerwony)')
+        b = axis.hist2d(calosc['x'], calosc['y'],bins=(rozmiarX/2,rozmiarY/2))
 
-sns.regplot(x=df['x'], y=df['y'], fit_reg=False, color='b', ax=a)
+        plt.pcolormesh(calosc, shading='gouraud')
 
-#
-#calosc
-#
+        plt.title('Histogram 2D mapy prawdopodobieństwa, 0 - ujemny potencjał, 255 - dodatni potencjał')
 
+        plt.colorbar(b[3], ax=axis)
+        plt.show()
+    elif wybor==8:
+        przygotujPlikPgm()
+    elif wybor==9:
 
+        #
+        #calosc
+        #
+        calosc['x'] = caloscX
+        calosc['y'] = caloscY
+        calosc.head()
 
-calosc['x'] = caloscX
-calosc['y'] = caloscY
+        #zagęszczenie
 
-calosc.head()
+        new, axxx = plt.subplots()
+        sns.kdeplot(calosc['x'],calosc['y'],n_levels=2, shade="True",ax=axxx).set_title('Mapa prawdopodobieństwa - zagęszczenie')
 
-'''
-plt.streamplot(calosc['x'],calosc['y'])
-streamplot - histogram przepływu'''
+        sns.kdeplot(df['x'],df['y'], n_levels=2, ax=axxx)
 
-
-#punkty
-figure, axes = plt.subplots()
-
-plt.title('Mapa prawdopodobieństwa punktowa')
-
-plt.plot(caloscX,caloscY,'.',color='black')
-
-#histogram 2D
-figur, axis = plt.subplots()
-
-
-b = axis.hist2d(calosc['x'], calosc['y'],bins=(rozmiarX/2,rozmiarY/2))
-
-
-plt.pcolormesh(calosc, shading='gouraud')
-
-plt.title('Histogram 2D mapy prawdopodobieństwa, 0 - ujemny potencjał, 255 - dodatni potencjał')
-
-plt.colorbar(b[3], ax=axis)
-
-#zagęszczenie
-
-new, axxx = plt.subplots()
-sns.kdeplot(calosc['x'],calosc['y'],n_levels=2, shade="True",ax=axxx).set_title('Mapa prawdopodobieństwa - zagęszczenie')
-
-sns.kdeplot(df['x'],df['y'], n_levels=2, ax=axxx)
-
-plt.show()
+        plt.show()
+    else:
+        print("Podaj poprawna opcje!")
+    wybor = int(input("Twoj wybor: "))
